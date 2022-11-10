f08075b0ceca001eeb1c1be1f39553f12a825412
Usama Ahmed Siddiquie <usiddique@cognitivehealthintl.com>
2022-11-10 15:55:13 +0500

    Change chromeium dependency

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:16
 WORKDIR /usr/src/app
 
 RUN apt update -y
-RUN apt install -y chromium-browser
+RUN apt install -y chromium
 
 COPY package*.json ./
 
--- index.js
+++ index.js
@@ -3,23 +3,24 @@ import puppeteer from 'puppeteer';
 async function run() {
     const browser = await puppeteer.launch(
         {
-            args: [ '--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream' ]
+            args: [ '--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream', '--proxy-server=' ]
         }
     );
 
-    for (let i = 0; i < 5; i++) {
-        const page = await browser.newPage();
-        await page.goto("https://kms-01.cognitivehealthintl.com/demo/");
-        
-        const roomInput = await page.$('#room_id');
-        await roomInput.click({ clickCount: 3 });
-        await roomInput.type('bot-test');
+    const page = await browser.newPage();
+    await page.goto("https://kms-01.cognitivehealthintl.com/demo/");
     
-        const startVideoButton = await page.$('#btnJoinCall')
-        startVideoButton.click()
-    
-        await page.waitForTimeout(10000);
-    }
+    const roomInput = await page.$('#room_id');
+    await roomInput.click({ clickCount: 3 });
+    await roomInput.type('bot-test-2');
+
+    const startVideoButton = await page.$('#btnJoinCall')
+    startVideoButton.click()
+
+    await page.waitForTimeout(10000);
+
+    const endCallButton = await page.$('#btn-leave-call')
+    endCallButton.click()
 
     browser.close()
 }
